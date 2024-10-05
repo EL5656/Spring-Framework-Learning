6793b2129c6f0d10617b8fb9ffe276e26b53a4f7
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-10-05 14:24:23 +0800

    Call method from Dev class

---
+++ src/main/java/org/example/Development/Dev.java
@@ -0,0 +1,12 @@
+package org.example.Development;
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+
+    public void build(){
+        System.out.println("working on awesome project");
+    }
+}
